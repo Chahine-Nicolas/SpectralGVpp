5d226bb1f00ec3788d6bfb8622e8966c99a73f30
chahine nicolas zede <chahine.nicolas.zede@gmail.com>
2026-02-23 16:00:55 +0100

    run file

---
+++ SpectralGV-main/.ipynb_checkpoints/run_save_pickle-checkpoint.sh
@@ -0,0 +1,6 @@
+python -m pdb evaluation/LoGG3D-Net/programation_rotation.py \
+    --root_path /lustre/fswork/projects/rech/dki/ujo91el/code \
+    --name_pickle blank.pickle \
+    --file_path_query_json /lustre/fsn1/worksf/projects/rech/dki/ujo91el/datas/lidarhd_v2/dsi_train_list.json \
+    --file_path_database_json /lustre/fsn1/worksf/projects/rech/dki/ujo91el/datas/lidarhd_v2/dsi_eval_list.json \
+    --bin_path /lustre/fsn1/worksf/projects/rech/dki/ujo91el/datas/lidarhd_v2/bin \
