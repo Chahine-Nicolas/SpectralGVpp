9c2a88242b4bc6e7355a4b7e19c2bb87cbf6e12a
chahine nicolas zede <chahine.nicolas.zede@gmail.com>
2026-02-23 16:13:44 +0100

    clean cache

--- SpectralGV-main/D3Feat_modif_1_laurent_1_moi/.ipynb_checkpoints/d3feat_standard-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-python -m pdb demo_registration_v4.py \
-    --dataset_root /lustre/fsn1/worksf/projects/rech/dki/ujo91el/datas/lidarhd_v2/ \
-    --path_model_D3Feat SpectralGV_D3Feat/models/D3Feat \
-    --dataset_type lidar_east \
-    --voxel_size 0.1 \
-    --reset_fichier True \
-    --file_rotation rotation
--- SpectralGV-main/D3Feat_modif_1_laurent_1_moi/.ipynb_checkpoints/run_demo-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-#rm *.ply
-rm demo_data_lidar/*.npz
-python demo_registration_dump_v3.py
